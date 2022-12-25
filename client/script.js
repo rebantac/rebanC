5ac466df9f586244d5c98508b94996bb2cd196e6
Rebanta Chakraborty <rebantachakraborty8@gmail.com>
2022-12-26 00:02:06 +0530

    initial commit

---
+++ client/script.js
@@ -0,0 +1,116 @@
+import bot from './assets/bot.svg';
+import user from './assets/user.svg';
+
+const form = document.querySelector('form');
+const chatContainer = document.querySelector('#chat_container');
+
+let loadInterval;
+
+function loader(element) { // loading the three dots when the bots finds the answer
+    element.textContent = 'RebanC is thinking';
+
+    loadInterval = setInterval(() => {
+        element.textContent += '.';
+
+        if (element.textContent === 'RebanC is thinking....') {
+            element.textContent = 'RebanC is thinking';
+        }
+    }, 300);
+}
+
+// this function ensures that the answer is typed letter-by-letter and not as a whole block
+function typeText (element, text) {
+    let index = 0; // index of character in 'text'
+
+    let interval = setInterval(() => {
+        if(index < text.length) {
+            element.innerHTML += text.charAt(index);
+            index++;
+        } else { // when all the text is written
+            clearInterval(interval);
+        }
+    }, 20);
+}
+
+// generating message for every single message to successfully map over them
+function generateUniqueId() {
+    const timestamp = Date.now(); // date is always unique
+    const randomNumber = Math.random(); // to make it more unique
+    const hexadecimalString = randomNumber.toString(16);
+
+    return `id-${timestamp}-${hexadecimalString}`;
+}
+
+// each block of qn and ans
+function chatStripe (isAi, value, uniqueId) {
+    return (
+        `
+            <div class="wrapper ${isAi && 'ai'}">
+                <div class="chat">
+                    <div class="profile">
+                        <img 
+                            src="${isAi ? bot : user}"
+                            alt="${isAi ? 'bot' : 'user'}"
+                        />
+                    </div>
+
+                    <div class="message" id=${uniqueId}> ${value} </div>
+                </div>
+            </div>
+        `
+    )
+}
+
+const handleSubmit = async (event) => {
+    event.preventDefault(); // prevents the browser from refreshing
+    
+    const data = new FormData(form);
+
+    // user's chatstripe
+    chatContainer.innerHTML += chatStripe(false, data.get('prompt'));
+
+    form.reset();
+
+    // bot's chatstripe
+    const uniqueId = generateUniqueId();
+    chatContainer.innerHTML += chatStripe(true, ' ', uniqueId);
+
+    chatContainer.scrollTop = chatContainer.scrollHeight; // puts the new msg in view
+    
+    const messageDiv = document.getElementById(uniqueId);
+
+    loader(messageDiv);
+
+    // fetch data from server -> bot's response
+    const response = await fetch('http://localhost:5000', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            prompt: data.get('prompt')
+        })
+    })
+
+    clearInterval(loadInterval);
+    messageDiv.innerHTML = '';
+
+    if (response.ok) {
+        const data = await response.json();
+        const parsedData = data.bot.trim();
+
+        typeText(messageDiv, parsedData);
+    } else {
+        const err = await response.text();
+
+        messageDiv.innerHTML = "Something went wrong";
+        alert(err);
+    }
+}
+
+form.addEventListener('submit', handleSubmit);
+form.addEventListener('keyup', (event) => {
+    if (event.keyCode === 13) {// '13' represents the 'enter' key
+        handleSubmit(event);
+    }
+})
---
+++ server/server.js
@@ -0,0 +1,52 @@
+// all the calls and config to call the openAI
+
+import express from "express";
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+import { Configuration, OpenAIApi } from "openai";
+
+dotenv.config();
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Dummy get()
+app.get('/', async (req, res) => {
+    res.status(200).send({
+        message: 'Hello from RebanC',
+    })
+});
+
+app.post('/', async (req, res) => {
+    try {
+        const prompt = req.body.prompt;
+
+        // Refer to -> https://beta.openai.com/playground/p/default-openai-api?lang=node.js&model=text-davinci-003
+        // for attributes
+        const response = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: `${prompt}`,
+            temperature: 0,
+            max_tokens: 3000,
+            top_p: 1,
+            frequency_penalty: 0.5,
+            presence_penalty: 0,
+        });
+
+        res.status(200).send({
+            bot: response.data.choices[0].text
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({ error })
+    }
+});
+
+app.listen(5000, () => console.log('Server is running on port http://localhost:5000'));
